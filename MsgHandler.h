f01f0aef0d14c28360b1adc3984b77f4f4fa6d36
jhsy0323 <45502917+jhsy0323@users.noreply.github.com>
2025-06-20 16:04:13 +0800

    Update MsgHandler.h

--- MsgHandler.h
+++ MsgHandler.h
@@ -1,12 +1,18 @@
 #pragma once
 
-#include <functional>
 #include <unordered_map>
 #include <map>
 #include <string>
 #include "any.h"
 #include "Log.h"
 #include "function_traits.h"
+#include "SupportAnyType.h"
+
+enum class EHandlerExecType {
+	sync,
+	async,
+	main,
+};
 
 class MsgHandler {
 #ifdef _DEBUG
@@ -21,34 +27,29 @@ public:
 		static MsgHandler i;
 		return &i;
 	}
-	void SetAsyncExecFunc(std::function<void(std::function<void()>)> func) { asyncExecFunc_ = func; }
-	void SetMainExecFunc(std::function<void(std::function<void()>)> func) { mainExecFunc_ = func; }
-
-	template<typename ...Args>
-	void TriggerMsg_(MsgType msg, Args... args) {
-		ExecMsgHandler(msg, defaultExecFunc_, args...);
+	void SetHandlerExecFunc(EHandlerExecType type, std::function<void(std::function<void()>)> func) { handlerExecFunc_[type] = func; }
+	template<typename Obj, typename F>
+	void RegisterMsgHandler_(MsgType msg, Obj* obj, F&& func, int priority = 0) {
+		globalMsgHandler_[msg][priority].emplace(obj, to_std_function(obj, std::forward<F>(func)));
 	}
 	template<typename ...Args>
-	void AsyncExecMsg_(MsgType msg, Args... args) {
-		if (!asyncExecFunc_) throw std::runtime_error("dont set async exec func");
-		ExecMsgHandler(msg, asyncExecFunc_, args...);
+	void TriggerMsg_(EHandlerExecType type, MsgType msg, Args&&... args) {
+		auto handlerExecFunc = handlerExecFunc_.at(type);
+		auto handlers = GetMsgHandler(msg, std::forward<Args>(args)...);
+		for (auto& handler : handlers) {
+			handlerExecFunc(handler);
+		}
 	}
-	template<typename ...Args>
-	void MainExecMsg_(MsgType msg, Args... args) {
-		if (!mainExecFunc_) throw std::runtime_error("dont set main exec func");
-		ExecMsgHandler(msg, mainExecFunc_, args...);
-	}
-	template<typename F>
-	void RegisterMsgHandler_(MsgType msg, InstID t, F func, int priority = 0) {
-		globalMsgHandler_[msg][priority].emplace(t, to_std_function(func));
-	}
-	template<typename Obj, typename Ret, typename... Args>
-	void RegisterMsgHandler_(MsgType msg, Obj* obj, Ret(Obj::*mem_func)(Args...), int priority = 0) {
-		globalMsgHandler_[msg][priority].emplace(obj, wrap(obj, mem_func));
-	}
-	template<typename Obj, typename Ret, typename... Args>
-	void RegisterMsgHandler_(MsgType msg, Obj* obj, Ret(Obj::*mem_func)(Args...) const, int priority = 0) {
-		globalMsgHandler_[msg][priority].emplace(obj, wrap(obj, mem_func));
+	template<typename Callback, typename ...Args>
+	void AsyncExecMsgAutoCallback_(MsgType msg, Callback callback, Args&&... args) {
+		auto handlers = GetMsgHandler(msg, std::forward<Args>(args)...);
+		auto func = [handlers, callback, this] {
+			for (auto& handler : handlers) {
+				handler();
+			}
+			handlerExecFunc_[EHandlerExecType::main](to_std_function(callback));
+		};
+		handlerExecFunc_[EHandlerExecType::async](func);
 	}
 	void Deregister_(InstID t) {
 		for (auto& msgHandler : globalMsgHandler_) {
@@ -61,14 +62,7 @@ public:
 	}
 private:
 	template<typename ...Args>
-	void ExecMsgHandler(MsgType msg, std::function<void(std::function<void()>)> exec, Args... args) {
-		auto handlers = GetMsgHandler(msg, args...);
-		for (auto& handler : handlers) {
-			exec(handler);
-		}
-	}
-	template<typename ...Args>
-	std::vector<std::function<void()>> GetMsgHandler(MsgType msg, Args... args) {
+	std::vector<std::function<void()>> GetMsgHandler(MsgType msg, Args&&... args) {
 		std::vector<std::function<void()>> ret;
 		auto it = globalMsgHandler_.find(msg);
 		if (it != globalMsgHandler_.end()) {
@@ -77,39 +71,26 @@ private:
 				HandlerMap& handlerMap = priorityEntry.second;
 				for (auto& handlerEntry : handlerMap) {
 					std::any& a = handlerEntry.second;
-					auto handler = AnyCastToHandler(a, args...);
-					ret.emplace_back(handler);
+					ret.emplace_back(wrap_to_std_fuction(a, std::forward<Args>(args)...));
 				}
 			}
 		}
 		return ret;
 	}
-	template<typename ...Args>
-	std::function<void()> AnyCastToHandler(std::any& a, Args... args) {
-		auto handler = std::bind([&a](Args... args) {
-			try {
-				auto func = std::any_cast<std::function<void(Args...)>>(a);
-				func(args...);
-			}
-			catch (std::runtime_error) {
-				auto func = std::any_cast<std::function<void()>>(a);
-				func();
-			}
-		}, args...);
-		return handler;
-	}
 private:
 	MsgHandler() = default;
 	using HandlerMap = std::unordered_map<InstID, std::any>;
 	using PriorityMap = std::map<int, HandlerMap, std::greater<int>>;
-	std::function<void(std::function<void()>)> defaultExecFunc_ = [](std::function<void()> func) { func(); };
-	std::function<void(std::function<void()>)> asyncExecFunc_;
-	std::function<void(std::function<void()>)> mainExecFunc_;
 	std::unordered_map<MsgType, PriorityMap> globalMsgHandler_;
+	using HandlerExecFunc = std::function<void(std::function<void()>)>;
+	std::unordered_map<EHandlerExecType, HandlerExecFunc> handlerExecFunc_ = { { EHandlerExecType::sync, [](std::function<void()> func) { func(); } } };
 };
 
 #define RegisterMsgHandler(msg, ...) MsgHandler::GetInstance()->RegisterMsgHandler_(#msg, this, __VA_ARGS__)
-#define TriggerMsg(msg, ...) MsgHandler::GetInstance()->TriggerMsg_(#msg, __VA_ARGS__)
-#define AsyncExecMsg(msg, ...)  MsgHandler::GetInstance()->AsyncExecMsg_(#msg, __VA_ARGS__)
-#define MainExecMsg(msg, ...)  MsgHandler::GetInstance()->MainExecMsg_(#msg, __VA_ARGS__)
+#define ExecMsg(execType, msg, ...) MsgHandler::GetInstance()->TriggerMsg_(execType, msg, __VA_ARGS__)
+#define SyncExecMsg(msg, ...) ExecMsg(EHandlerExecType::sync, #msg, __VA_ARGS__)
+#define AsyncExecMsg(msg, ...)  ExecMsg(EHandlerExecType::async, #msg, __VA_ARGS__)
+#define MainExecMsg(msg, ...)  ExecMsg(EHandlerExecType::main, #msg, __VA_ARGS__)
+
+#define AsyncExecMsgAutoCallback(msg, ...)  MsgHandler::GetInstance()->AsyncExecMsgAutoCallback_(#msg, __VA_ARGS__)
 #define Deregister() MsgHandler::GetInstance()->Deregister_(this)
